e48401e0e123999195ef84f273c5bc41aea6854e
rvweerakoon12 <ruwanthivenisha@gmail.com>
2022-02-06 00:09:21 +0530

    gallery page

--- gallerypage/gallery.js
+++ gallerypage/gallery.js
@@ -2,15 +2,23 @@
 const filterItem = document.querySelector(".items");
 const filterImg = document.querySelectorAll(".image");
 
-
 window.onload = ()=>{ //once window loarded
-    filterItem.onClick = (selectedItem)=>{ //when user clicked on filterItem div
+    filterItem.onclick = (selectedItem)=>{ //when user clicked on filterItem div
         if(selectedItem.target.classList.contains("item")){//if user click the element has.item class
-           filterItem.querySelector(".active").classList.remove("active");
-        }
-         else{
-                console.log("false");
+           filterItem.querySelector(".active").classList.remove("active"); //remove the active class
+           selectedItem.target.classList.add("active");
+           let filterName = selectedItem.target.getAttribute("data-name");
+           filterImg.forEach((image)=>{
+               let filterImages = image.getAttribute("data-name");
+               if((filterImages==filterName)|| filterName=="all"){
+                image.classList.remove("hide");
+                image.classList.add("show");
+               }
+               else{
+                image.classList.add("hide");
+                image.classList.remove("show");
+               }
+            });
             }
-
         }
     }
