b697039aa753b9ce4d815c9786b0072b5eb3c3bd
wcchathu <wccwijemanna@students.nsbm.ac.lk>
2022-02-07 13:18:39 +0530

    gallery commit

--- gallery.js
+++ gallery.js
@@ -35,18 +35,18 @@ window.onload = ()=>{ //once window loarded
         shadow = document.querySelector(".shadow");
         
         function preview(element){
-          //once user click on any image then remove the scroll bar of the body, so user can't scroll up or down
+         
           document.querySelector("body").style.overflow = "hidden";
-          let selectedPrevImg = element.querySelector("img").src; //getting user clicked image source link and stored in a variable
-          let selectedImgCategory = element.getAttribute("data-name"); //getting user clicked image data-name value
-          previewImg.src = selectedPrevImg; //passing the user clicked image source in preview image source
-          categoryName.textContent = selectedImgCategory; //passing user clicked data-name value in category name
-          previewBox.classList.add("show"); //show the preview image box
-          shadow.classList.add("show"); //show the light grey background
-          closeIcon.onclick = ()=>{ //if user click on close icon of preview box
-            previewBox.classList.remove("show"); //hide the preview box
-            shadow.classList.remove("show"); //hide the light grey background
-            document.querySelector("body").style.overflow = "auto"; //show the scroll bar on body
+          let selectedPrevImg = element.querySelector("img").src; 
+          let selectedImgCategory = element.getAttribute("data-name"); 
+          previewImg.src = selectedPrevImg; 
+          categoryName.textContent = selectedImgCategory; 
+          previewBox.classList.add("show"); 
+          shadow.classList.add("show"); 
+          closeIcon.onclick = ()=>{ 
+            previewBox.classList.remove("show");
+            shadow.classList.remove("show"); 
+            document.querySelector("body").style.overflow = "auto"; 
           }
         }
         
