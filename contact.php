76e4660ae11bcc36faff47ef0781d01cf3eeb01a
Chathurangi Wijemanna <96134411+Chathurangiw@users.noreply.github.com>
2022-02-24 18:41:39 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -18,6 +18,8 @@
             <input type="text" name="name" id="name">
             <label for="email">Email:</label>
             <input type="email" name="email" id="email">
+             <label for="password">Password:</label>
+            <input type="password" name="password" id="password">
             <label for="subject">Subject:</label>
             <input type="text" name="subject" id="subject">
             <label for="message">Message</label>
